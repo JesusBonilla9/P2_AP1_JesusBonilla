d330833f75f0a78e7aafad3bd24a9e9357d5ecf1
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2025-11-03 18:57:15 -0400

    Se crearon los Models

---
+++ P2_AP1_JesusBonilla/Models/Pedidos.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P2_AP1_JesusBonilla.Models
+{
+    public class Pedidos
+    {
+        [Key]
+        public int PedidoId { get; set; }
+        [Required(ErrorMessage = "La fecha es obligatoria.")]
+        public DateTime Fecha { get; set; }
+        [Required(ErrorMessage = "El Nombre del cliente es obligatorio")]
+        [MaxLength(70)]
+        public string NombreCliente { get; set; }
+
+    }
+}
---
+++ P2_AP1_JesusBonilla/Models/PedidosDetalle.cs
@@ -0,0 +1,11 @@
+﻿namespace P2_AP1_JesusBonilla.Models
+{
+    public class PedidosDetalle
+    {
+        public int Id { get; set; }
+        public int PedidoId { get; set; }
+        public int ComponenteId { get; set; }
+        public int Cantidad { get; set; }
+        public int Precio { get; set; }
+    }
+}
